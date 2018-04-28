f48b83595f05655dbb22c977899e2652d8108f4d
CassadyBlake <cassady.lillstrom@gmail.com>
2018-04-28 12:47:15 -0700

    user story 1 init auth: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,7 +5,15 @@ class Kele
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
+    response = self.class.post( '/sessions',
+      body: { "email": email,
+              "password": password
+            })
+    if response.code != 200
+      raise 'invalid email or password'
+    end
 
+    @auth = response['auth_token']
   end
 
 end
